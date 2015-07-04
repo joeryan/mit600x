66f7f571581ce8d8af920d7a0b9bccaf46a4bfc6
Joe Ryan <joe@clanryan.us>
2015-07-04 00:37:12 -0400

    week 2, set 2/Prob 2: minimum payment to pay of a debt in less than a year.

---
+++ minpmt.py
@@ -0,0 +1,21 @@
+# edX MIT6.00.1x programming in python
+# problem set 2, week 2, problem 2
+# minumum payment to pay off debt in one year
+
+annualInterestRate = 0.2
+balance = 4114.0
+
+monthlyInterest = annualInterestRate / 12
+remainingBal = balance
+monthlyPmt = 10
+
+while (remainingBal > 0):
+    for month in range(1,13):
+        remainingBal -= monthlyPmt
+        remainingBal += monthlyInterest * remainingBal
+        
+    if remainingBal > 0:
+        remainingBal = balance
+        monthlyPmt += 10
+
+print "Lowest Payment: %d" % monthlyPmt
